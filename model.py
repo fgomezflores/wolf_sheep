ba3388518ea667fb3d3b4466962bcaea5202c786
fgflores <fgomezflores@gmail.com>
2023-07-09 16:47:42 +0200

    ok

---
+++ model.py
@@ -0,0 +1,11 @@
+"""
+Wolf-Sheep Predation Model
+================================
+
+Replication of the model found in NetLogo:
+    Wilensky, U. (1997). NetLogo Wolf Sheep Predation model.
+    http://ccl.northwestern.edu/netlogo/models/WolfSheepPredation.
+    Center for Connected Learning and Computer-Based Modeling,
+    Northwestern University, Evanston, IL.
+"""
+
